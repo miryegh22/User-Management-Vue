b2614462c7cf7b714a401359fc2bcc925b2c0de8
miryegh22 <111610594+miryegh22@users.noreply.github.com>
2022-09-29 09:34:06 +0400

    Update AddUser.vue

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -6,37 +6,38 @@
         <div class="flex flex-col gap-5">
             <div>
                 <label>Firstname</label>
-                <input type="text" v-model="addName"
-                    class="  w-full py-1 px-2 mb-1  border border-green-900 rounded" required>
+                <input type="text" v-model="addName" class="  w-full py-1 px-2 mb-1  border border-green-900 rounded"
+                    required>
                 <label>Lastname</label>
                 <input type="text" v-model="addLastName"
                     class="block  w-full py-1 px-2 mb-1  border border-green-900 rounded" required>
             </div>
             <div>
                 <label>Email</label>
-                <input type="email" v-bind:value="addEmail" @input="Email"
-                    class=" w-full py-1 px-2 mb-1  border border-green-900 rounded"  required>
-                  
+                <input type="email" v-model="addEmail" @input="Email"
+                    class=" w-full py-1 px-2 mb-1  border border-green-900 rounded" required>
+
             </div>
             <div>
                 <label>Username</label>
                 <input type="text" v-model="addUsername"
-                    class="block  w-full py-1 px-2 mb-1 text-base  border border-green-900 rounded"
-                    required>
+                    class="block  w-full py-1 px-2 mb-1 text-base  border border-green-900 rounded" required>
             </div>
             <div>
                 <label>Phone</label>
                 <div class="flex">
-                <span>+</span>
-                <input type="text" class=" w-full py-1 px-2 mb-1 text-base border border-green-800 rounded" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');"   pattern="\d*"  minlength="12" maxlength="12"  v-bind:value="addPhone" @input="Phone"  placeholder="1234 1234 1234 " required>
-               
+                    <span>+</span>
+                    <input type="text" class=" w-full py-1 px-2 mb-1 text-base border border-green-800 rounded"
+                        oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');"
+                        pattern="\d*" minlength="12" maxlength="12" v-bind:value="addPhone" @input="Phone"
+                        placeholder="1234 1234 1234 " required>
+
                 </div>
             </div>
         </div>
         <div class="flex justify-center gap-4 mt-2">
-            <button  @click="cancel" class="mt-1">Cancel</button>
-            <button  class=" text-center  py-1 px-3  bg-green-500 text-white hover:green-600"
-                @click="add" >Add
+            <button @click="cancel" class="mt-1">Cancel</button>
+            <button class=" text-center  py-1 px-3  bg-green-500 text-white hover:green-600" @click="add">Add
             </button>
         </div>
     </form>
@@ -53,56 +54,59 @@ export default {
             addEmail: '',
             addUsername: '',
             addPhone: '',
-            msg:''
-           
+
+
         }
     },
-    
+
     methods: {
-        Email(event){
-            this.addEmail=event.target.value;
-            const reg=/^([a-zA-Z0-9/._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-            if(reg.test(this.addEmail)){
-                event.target.style='border: 1px solid green;'
+        Email(event) {
+            this.addEmail = event.target.value;
+            const reg =   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+            if (reg.test(this.addEmail)) {
+                event.target.style = 'border: 1px solid green;'
             }
-            else{
-                event.target.style="border: 2px solid red;"
-            }
-        },
-        Phone(event){
-            this.addPhone=event.target.value;
-            if(this.addPhone.length<12){
-                event.target.style="border: 2px solid red;"}
-            else{
-                event.target.style='border: 1px solid green;'
+            else {
+                event.target.style = "border: 2px solid red;"
             }
         },
-        cancel(){
-            router.replace({path:'/'})
+        Phone(event) {
+            this.addPhone = event.target.value;
+            if (this.addPhone.length < 12) {
+                event.target.style = "border: 2px solid red;"
+            }
+            else {
+                event.target.style = 'border: 1px solid green;'
+            }
         },
-         add() {
-            const reg=/^([a-zA-Z0-9/._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-            if(this.addName.length!=0 && this.addLastName.length!=0 && reg.test(this.addEmail) && this.addUsername.length!=0 && this.addPhone.length===12){
-                router.replace({path:'/'})
-            const newrow = { image: "https://robohash.org/GDZ.png?set=set2", firstName: this.addName, lastName: this.addLastName, email: this.addEmail, username: this.addUsername, phone:'+'+ this.addPhone };
-            const user = JSON.parse(localStorage.getItem("userTable"));
-            user.push(newrow);
-            localStorage.setItem("userTable", JSON.stringify(user));
-            fetch(`https://dummyjson.com/users/add`, {
-                method: 'POST',
-                body: JSON.stringify({
-                   newrow
-                }),
-            }).then(res => {
-                if (res.ok) {
-                    console.log({
-                       newrow
-                    });
-                }
-            })
-        
-    }}}
+        cancel() {
+            router.replace({ path: '/' })
+        },
+        add() {
+            const reg =   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+            if (this.addName.length != 0 && this.addLastName.length != 0 && reg.test(this.addEmail) && this.addUsername.length != 0 && this.addPhone.length === 12) {
+                router.replace({ path: '/' })
+                const newrow = { image: "https://robohash.org/GDZ.png?set=set2", firstName: this.addName, lastName: this.addLastName, email: this.addEmail, username: this.addUsername, phone: '+' + this.addPhone };
+                const user = JSON.parse(localStorage.getItem("userTable"));
+                user.push(newrow);
+                localStorage.setItem("userTable", JSON.stringify(user));
+                fetch(`https://dummyjson.com/users/add`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        newrow
+                    }),
+                }).then(res => {
+                    if (res.ok) {
+                        console.log({
+                            newrow
+                        });
+                    }
+                })
+            }
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
+
 </style>
